c48e202cb5d301f3c502a04d124d0f3f415895f3
Emry Daniel <emrydaniel1@gmail.com>
2026-01-23 23:22:35 +0800

    Added increment Memz

---
+++ src/main/java/Memz.java
@@ -0,0 +1,12 @@
+public class Memz {
+    public static void main(String[] args) {
+        String logo = "____________________________________________________________\n"
+                + "Hello! I'm Memz!\n"
+                + "Want a cookie?\n"
+                + "Dowan give then how? Cry lor\n"
+                + "____________________________________________________________\n"
+                + "Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________\n";
+        System.out.println(logo);
+    }
+}
