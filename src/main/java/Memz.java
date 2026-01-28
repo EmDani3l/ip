0d0228d9315ec902fd50964a3b4c63a5b8df41ab
Emry Daniel <emrydaniel1@gmail.com>
2026-01-28 18:42:57 +0800

    Implemented Level 1. Echo to skeletal code

--- src/main/java/Memz.java
+++ src/main/java/Memz.java
@@ -1,12 +1,28 @@
+import java.util.Scanner;
+
 public class Memz {
     public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
         String logo = "____________________________________________________________\n"
                 + "Hello! I'm Memz!\n"
-                + "Want a cookie?\n"
-                + "Dowan give then how? Cry lor\n"
-                + "____________________________________________________________\n"
-                + "Bye. Hope to see you again soon!\n"
+                + "What can I do for you?\n"
                 + "____________________________________________________________\n";
         System.out.println(logo);
+
+        while (true) {
+            String input = in.nextLine();
+            System.out.println("___________________________________________________________\n");
+
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!\n"
+                + "___________________________________________________________");
+                break;
+            } else {
+                System.out.println(input);
+                System.out.println("___________________________________________________________");
+            }
+        }
     }
 }
