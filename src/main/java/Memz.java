a9bfb8222d1464a14794c74b7eed7fcef30f5574
Emry Daniel <emrydaniel1@gmail.com>
2026-02-02 22:58:53 +0800

    Properly removed Duke from README portion. Added todo, deadline, event classes and functionality.

--- src/main/java/Memz.java
+++ src/main/java/Memz.java
@@ -42,10 +42,23 @@ public class Memz {
                 tasks[idx].unmarkDone();
                 System.out.println("SEE, you're not done yet:");
                 System.out.println("  " + tasks[idx]);
-            } else {
-                tasks[taskCount] = new Task(input);
+            } else if (input.startsWith("todo")) {
+                String description = input.substring(5);
+                tasks[taskCount] = new Todo(description);
                 taskCount++;
-                System.out.println("added: " + input);
+                System.out.println("Wah think u big isit. I add:\n " + tasks[taskCount - 1] + "\n Now you have " + taskCount + " tasks in the list.");
+            } else if (input.startsWith("deadline")) {
+                String[] parts = input.substring(9).split(" /by ");
+                tasks[taskCount] = new Deadline(parts[0], parts[1]);
+                taskCount++;
+                System.out.println("Careful ah:\n" + tasks[taskCount -1] + "\n Now you have " + taskCount + " tasks in the list.");
+            } else if (input.startsWith("event")) {
+                String[] parts = input.substring(6).split(" /from | /to ");
+                tasks[taskCount] = new Event(parts[0], parts[1], parts[2]);
+                taskCount++;
+                System.out.println("Bye time:\n" + tasks[taskCount -1] + "\n Now you have " + taskCount + " tasks in the list.");
+            } else {
+                System.out.println("TF did u just say? Try again.");
             }
             System.out.println("___________________________________________________________");
         }
